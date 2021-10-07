e33134722f2313126c4fee1b336d15cf0880eba1
groiq <35578027+groiq@users.noreply.github.com>
2021-10-07 12:41:23 +0200

    added test for sorter to trace bucketsort

--- lib/src/test/java/exercise/arraysTDD/ArrayOperationsAdditionalTests.java
+++ lib/src/test/java/exercise/arraysTDD/ArrayOperationsAdditionalTests.java
@@ -5,6 +5,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayOperationsAdditionalTests {
 
+    /**
+     * sort a reversed array of ints 0-7, for clear tracing in BucketSort
+     */
+    @org.junit.jupiter.api.Test
+    void testSortingReversedArray() {
+        int[] givenArray = { 7, 6, 5, 4, 3, 2, 1, 0 };
+        ArrayOperations givenArrayOps = new ArrayOperations(givenArray);
+
+        int[] actual = givenArrayOps.sort();
+
+        int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7 };
+        assertArrayEquals(expected, actual);
+    }
+
     @org.junit.jupiter.api.Test
     void sortingShouldHandleDuplicates() {
         int[] givenArray = { 3, 1, 7, 3 };
